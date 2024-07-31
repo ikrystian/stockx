b04ee0b82bbca393151241f356a2a1bc22a1c723
Krystian <krystian.kuzminski@kolektywkreatywny.pl>
2024-07-31 13:11:26 +0200

    Add html skeleton

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,28 @@
-<script lang="ts">
-
-</script>
-
 <main>
+    <div class="container">
+        <div class="slider">slider</div>
+        <div class="form">form</div>
+    </div>
+
+    <div class="container">
+        <div class="title">section title</div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            first fow
+        </div>
+        <div class="row">
+            second row
+        </div>
+    </div>
+
+
+    <div class="bottom-section">
+        Bottom section
+    </div>
 
 </main>
-
-<style>
-
-</style>
+<footer class="footer">
+    credit cards
+</footer>
