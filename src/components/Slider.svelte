5772408ea82f9e82799c4ba756f501fca1fc7c31
Krystian <krystian.kuzminski@kolektywkreatywny.pl>
2024-08-05 21:12:34 +0200

    Add RWO to top section

--- src/components/Slider.svelte
+++ src/components/Slider.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import { fade } from 'svelte/transition';
-    import { onMount } from 'svelte';
+    import {fade} from 'svelte/transition';
+    import {onMount} from 'svelte';
 
     let photos = [
         'src/assets/slider_1.webp',
@@ -21,61 +21,74 @@
     });
 </script>
 
-<style>
-    .slider {
-        position: relative;
-        width: 100%;
-        height: 300px;
-        max-width: 800px;
-        margin: auto;
-        overflow: hidden;
+<style lang="scss">
+  .slider-section {
+    max-width: 480px;
+    margin-inline: auto;
+    margin-bottom: 2rem;
+
+    @media (width > 768px) {
+      width: 480px;
+      max-width: 100%;
     }
-    .photo {
-        width: 100%;
-        max-width: 100%;
-        display: block;
-        height: auto;
-        max-height: 100%;
+  }
+
+  .slider {
+    position: relative;
+    width: 100%;
+    aspect-ratio: 1;
+    overflow: hidden;
+  }
+
+  .photo {
+    width: 100%;
+    max-width: 100%;
+    display: block;
+    height: auto;
+    max-height: 100%;
+  }
+
+  .thumbnails {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .thumbnail {
+    width: 132px;
+    height: 132px;
+    cursor: pointer;
+    transition: transform 0.3s linear;
+    border-radius: 20px;
+    overflow: hidden;
+    border: 1px solid var(--primary-color);
+
+    img {
+      width: 100%;
+      height: auto;
     }
-    .thumbnails {
-        display: flex;
-        justify-content: center;
-        margin-top: 10px;
+
+    &:not(.active) img {
+      opacity: 0.5;
     }
-    .thumbnail {
-        width: 60px;
-        height: 60px;
-        margin: 0 5px;
-        cursor: pointer;
-        transition: transform 0.3s ease;
-    }
-    .thumbnail:hover {
-        transform: scale(1.1);
-    }
-    .thumbnail img {
-        width: 100%;
-        height: auto;
-        border: 2px solid transparent;
-    }
-    .thumbnail.active img {
-        border-color: #333;
-    }
+  }
 </style>
 
-<div class="slider">
-    {#each photos as photo, index (photo)}
-        {#if index === currentIndex}
-            <img src={photo} alt="Photo" class="photo" transition:fade />
-        {/if}
-    {/each}
-</div>
-<div class="thumbnails">
-    {#each photos as photo, index}
-        <div
-                class="thumbnail {index === currentIndex ? 'active' : ''}"
-                on:click={() => selectPhoto(index)}
-        >
-            <img src={photo} alt="Thumbnail" />
-        </div>
-    {/each}
-</div>
+<section class="slider-section">
+    <div class="slider">
+        {#each photos as photo, index (photo)}
+            {#if index === currentIndex}
+                <img src={photo} alt="Photo" class="photo" transition:fade>
+            {/if}
+        {/each}
+    </div>
+    <div class="thumbnails">
+        {#each photos as photo, index}
+            <button
+                    class="thumbnail {index === currentIndex ? 'active' : ''}"
+                    on:click={() => selectPhoto(index)}
+            >
+                <img src={photo} alt="Thumbnail"/>
+            </button>
+        {/each}
+    </div>
+</section>
