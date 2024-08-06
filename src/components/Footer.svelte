b4bd6021669460ad3cede9eb8ebe9b3297846b88
Krystian <krystian.kuzminski@kolektywkreatywny.pl>
2024-08-06 15:14:51 +0200

    Move footer styles to separate component Change styles to scss and BEM

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,19 @@
 <footer class="footer">
-    <p>We accept the following credit cards</p>
-    <img alt="credit cards image" src="src/assets/footer.webp">
+    <p class="footer__text">We accept the following credit cards</p>
+    <img alt="credit cards image" class="footer__image" src="src/assets/footer.webp">
 </footer>
+
+<style lang="scss">
+  .footer {
+    text-align: center;
+
+    &__image {
+      margin-inline: auto;
+    }
+
+    &__text {
+      font-size: 0.75rem;
+      margin-bottom: 0.25rem;
+    }
+  }
+</style>
